0c1e3f63b6a3cab80916134900e2ac6712ffba46
Alan Soto <alansfj348@gmail.com>
2021-06-22 18:53:49 -0500

    last component: MaterialUI

---
+++ src/components/MaterialUI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MediaCard from './MaterialCard';
+
+export default function MaterialUI() {
+    return (
+        <>
+            <h2>Material UI</h2>
+            <MediaCard/>
+            </>
+    )
+}
